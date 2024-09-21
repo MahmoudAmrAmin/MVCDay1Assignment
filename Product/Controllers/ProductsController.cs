f2ae5ed7846e5fc0fe7ec412e5fd7f420e5ad423
Mahmoud Amr <mahmoudamr500@gmail>
2024-09-21 22:19:11 +0300

    add all files of answer

---
+++ Product/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product.Models;
+
+
+namespace Product.Controllers
+{
+    public class ProductsController:Controller
+    {
+        // /Products/ShowAllProducts
+        public IActionResult ShowAllProducts ()
+        {
+
+            ProductsBL products = new ProductsBL ();
+
+            List<Products> resultModel = products.GetAllProducts();
+
+            return View("showAllProducts", resultModel); 
+        }
+
+
+        // Products/GetProductDetails?id=1
+        public IActionResult GetProductDetails(int id )
+        {
+
+            ProductsBL product = new ProductsBL ();  
+
+            Products ProductDetailModel = product.GetProductById(id);
+
+            return View("GetProductDetails", ProductDetailModel); 
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Product/Models/ProductsBL.cs
@@ -0,0 +1,65 @@
+﻿using System.Numerics;
+using System.Reflection;
+
+namespace Product.Models
+{
+    public class ProductsBL
+    {
+        List<Products> products;
+
+        public ProductsBL()
+        {
+            products = new List<Products>();
+
+            products.Add(new Products()
+            {
+                Id = products.Count+1,
+                Name = "iphone",
+                Description = "Mobile phone.",
+                ImageUrl = "iphone.jpg",
+                Price = 10000m 
+            });
+            products.Add(new Products()
+            {
+                Id = products.Count + 1,
+                Name = "tv",
+                Description = "tv screen",
+                ImageUrl = "tv.jpg",
+                Price = 15000m
+            });
+
+            products.Add(new Products()
+            {
+                Id = products.Count + 1,
+                Name = "laptop",
+                Description = "lap top divce",
+                ImageUrl = "laptop.jpg",
+                Price = 30000m
+            });
+
+        }
+
+        public List<Products> GetAllProducts()
+        {
+
+            return products;
+        }
+
+        public Products GetProductById(int id)
+        {
+
+            return products.FirstOrDefault(product => product.Id == id); 
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
